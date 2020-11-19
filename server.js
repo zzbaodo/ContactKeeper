e19bb07308f8303c0890441ce656f568bbab00b3
Bao <zzbaodo@gmail.com>
2020-11-18 23:03:33 -0800

    Add .gitignore

--- server.js
+++ server.js
@@ -1,28 +1,26 @@
-const express = require('express');
-const cors = require('cors')
-const connectDB = require('./config/ds')
-const path = require('path')
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/ds");
+const path = require("path");
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json({extended:false}))
-connectDB()
-app.use('/api/users', require('./routes/users'))
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/contacts', require('./routes/contacts'))
-
+app.use(cors());
+app.use(express.json({ extended: false }));
+connectDB();
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
 
 //Serve static assets in production
-if(process.env.NODE_ENV === 'production'){
-    //Set static folder
-    app.use(express.static('client/build'))
-    app.get('*', (req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    })
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 const PORT = process.env.PORT || 5000;
 
-
-app.listen(PORT, ()=> console.log(`Server starts on port: ${PORT}`))
+app.listen(PORT, () => console.log(`Server starts on port: ${PORT}`));
